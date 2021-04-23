d63e29614bc5ad85c07c5ec3dafeb0b267a1a7c7
Ben Jones <ben@pseudonym.party>
2021-04-22 23:42:32 -0700

    test: integration test unathenticated executor

--- packages/contracts/contracts/optimistic-ethereum/OVM/execution/OVM_ExecutionManager.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/execution/OVM_ExecutionManager.sol
@@ -1893,7 +1893,7 @@ contract OVM_ExecutionManager is iOVM_ExecutionManager, Lib_AddressResolver {
     )
         override
         external
-        onlyCallableBy(resolve("OVM_UpgradeExecutor"))
+        onlyCallableBy(0x420000000000000000000000000000000000000A)
     {
         _checkAccountLoad(_address);
         ovmStateManager.putAccountCode(_address, _code);
@@ -1913,7 +1913,7 @@ contract OVM_ExecutionManager is iOVM_ExecutionManager, Lib_AddressResolver {
     )
         override
         external
-        onlyCallableBy(resolve("OVM_UpgradeExecutor"))
+        onlyCallableBy(0x420000000000000000000000000000000000000A)
     {
         _putContractStorage(
             _address,
